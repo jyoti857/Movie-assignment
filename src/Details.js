89c56432757f0e7207bb4c903354e48812b23bb4
Jyoti857 <jyotiranjan857@gmail.com>
2022-06-20 19:26:54 +0530

    back button in the details page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
-import { View, Button, StyleSheet, ScrollView, TextInput } from "react-native";
+import { View, StyleSheet, ScrollView, TextInput } from "react-native";
+import {Button} from 'react-native-paper'
 import CustomCard from './Components/Card';
 
-export default function Details({route}){
+export default function Details({ navigation, route}){
   const {uri, movieTitle, year, type} = route.params;
+  const onBackPress = () => {
+    navigation.navigate("Home");
+  }
   return(
     <View>
+      <Button
+        style = {{marginTop: 40, marginLeft: 10, width: 100 }}
+        onPress={() => navigation.navigate('Home')}
+      >Back</Button>
       <CustomCard 
         uri={uri} 
         movieTitle={movieTitle} 
